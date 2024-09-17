0ca01de37653a58a45834f855dafb18d56ade505
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2024-09-16 21:26:47 -0400

    FEAT: Add like functionality for projects
    When star icon is clicked it gets hightlighted

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-const log = console.log;
+const stars = document.querySelectorAll('.star');
+stars.forEach((star) => star.addEventListener('click', event => {
+    const svg = star.querySelector('svg');
+    svg.classList.toggle('liked');
+}))
